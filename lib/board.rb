b99998720de2bd55709f021086b73d4f41d83e8d
Luan Santos <cfcluan@gmail.com>
2013-01-29 22:53:07 -0800

    Simple board with error handling

---
+++ lib/board.rb
@@ -0,0 +1,23 @@
+class Board
+  attr_reader :cells
+
+  def initialize map
+    validate_board map
+    @cells = map.chomp.lines.map do |line|
+      line.chomp.split(//)
+    end
+  end
+
+  private
+
+  def validate_board map
+    raise InvalidBoard if map.empty?
+    raise InvalidBoard if /[^\^\$\.#\n]/ =~ map
+    raise NoEntryFound unless /\^/ =~ map
+    raise NoExitFound unless /\$/ =~ map
+  end
+
+  class InvalidBoard < StandardError ; end
+  class NoEntryFound < StandardError ; end
+  class NoExitFound < StandardError ; end
+end
---
+++ spec/lib/board_spec.rb
@@ -0,0 +1,50 @@
+require './lib/board'
+
+describe Board do
+  describe "creation" do
+    context "when passing in '^$'" do
+      subject { Board.new "^$\n" }
+      its(:cells) { should == [['^', '$']] }
+    end
+
+    context "when passing in a complex map" do
+      subject { Board.new "^..#\n..#\$\n" }
+      its(:cells) { should == [["^", ".", ".", "#"], [".", ".", "#", "$"]] }
+    end
+
+    # it "handles double entry points"
+    # it "handles double exit"
+
+    context "when passing an invalid map" do
+      it "raises an error for the empty string" do
+        -> do
+          Board.new ""
+        end.should raise_error Board::InvalidBoard
+      end
+
+      it "raises an error for a map with unknown characters" do
+        -> do
+          Board.new "^hakuna.matata$"
+        end.should raise_error Board::InvalidBoard
+      end
+
+      it "raises an error a map without both entry point and exit" do
+        -> do
+          Board.new ".\n"
+        end.should raise_error Board::NoEntryFound
+      end
+
+      it "raises an error for a map without entry point" do
+        -> do
+          Board.new "#..#\n..#\$\n"
+        end.should raise_error Board::NoEntryFound
+      end
+
+      it "raises an error for a map without an exit" do
+        -> do
+          Board.new "^..#\n..##\n"
+        end.should raise_error Board::NoExitFound
+      end
+    end
+  end
+end
